af04f4e112b555a0e3a5feed66444a5151b1f6e5
kazuki-afb <kauzki_afb@yahoo.co.jp>
2021-04-05 21:50:10 +0900

    Initial commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    //method create
+    public function index()
+    {
+        return 'ハロワ！';
+    }
+}
